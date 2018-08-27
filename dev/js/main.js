205cb45471fe2ecfd8e2d42492b87b6023ed0d5a
ubarum004 <a538642@hotmail.com>
2018-08-27 16:16:33 -0700

    website1

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,20 +3,4 @@ jQuery(document).ready(function($){
 	preloader.fadeOut('slow', function() {
 		$(this).remove();
 	});
-	var windowHeight = $(window).height();
-	var container1Height = $("#container-1").height();
-	if(windowHeight > container1Height){
-		$("#container-1").height(windowHeight+45);
-	}
-	$(window).resize(function(){
-		$("#container-1").height("100%");
-		if($(window).height() >= 927){
-			var windowHeight = $(window).height();
-			var container1Height = $("#container-1").height();
-			if(windowHeight > container1Height){
-				$("#container-1").height(windowHeight);
-			}
-		} 
-		
-	});
 });
